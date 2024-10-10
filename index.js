e783713e59dd3c45966af892384af62967087e42
Dan <danporeda@yahoo.com>
2024-10-09 23:22:57 -0400

    mongoose connection set in index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/pow-hunt');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+  console.log("Database connected");
+});
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.listen(3000, () => {
-  console.log('Serving on Port 3000')
-});
-
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log('Serving on Port 3000')
 });
