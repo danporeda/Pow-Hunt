7703367b6b3d6fff21fef8f9cf4761e9033fb1a4
Dan <danporeda@yahoo.com>
2024-11-29 14:15:21 -0500

    allows multiple files to upload with multer

--- routes/mountains.js
+++ routes/mountains.js
@@ -4,10 +4,15 @@ const mountains = require('../controllers/mountains');
 const catchAsync = require('../utils/catchAsync');
 const Mountain = require('../models/mountain');
 const { isLoggedIn, validateMountain, isAuthor } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/')
   .get(catchAsync(mountains.index))
-  .post(isLoggedIn, validateMountain, catchAsync(mountains.createMountain));
+  // .post(isLoggedIn, validateMountain, catchAsync(mountains.createMountain));
+  .post(upload.array('image'), (req, res) => {
+    console.log(req.body, req.files);
+  })
 
 router.get('/new', isLoggedIn, mountains.renderNewForm);
 
