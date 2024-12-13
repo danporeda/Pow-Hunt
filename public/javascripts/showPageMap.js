3b31fe45d4550fbf2539aa535780300049775a3f
Dan <danporeda@yahoo.com>
2024-12-13 06:15:56 -0500

    corrected another typo -> campground to mountain, in showPageMap.js

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,16 +3,16 @@ maptilersdk.config.apiKey = maptilerApiKey;
 const map = new maptilersdk.Map({
     container: 'map',
     style: maptilersdk.MapStyle.BRIGHT,
-    center: campground.geometry.coordinates, // starting position [lng, lat]
+    center: mountain.geometry.coordinates, // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
 
 new maptilersdk.Marker()
-    .setLngLat(campground.geometry.coordinates)
+    .setLngLat(mountain.geometry.coordinates)
     .setPopup(
         new maptilersdk.Popup({ offset: 25 })
             .setHTML(
-                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+                `<h3>${mountain.title}</h3><p>${mountain.location}</p>`
             )
     )
     .addTo(map)
